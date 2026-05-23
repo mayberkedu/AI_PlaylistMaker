a17d0badf13471a469abac51cabeda9783f0dbed
Mehmet Ayberk Güneş <mayberkgunesedu@gmail.com>
2026-05-23 11:04:38 +0300

    test kodları temizlendi

--- py_test.py
+++
@@ -1,3 +0,0 @@
-import sys
-with open('out2.txt', 'w') as f:
-    f.write('Python is writing to file')
--- track_test.py
+++
@@ -1,4 +0,0 @@
-from data_collection import get_spotify_client
-sp = get_spotify_client()
-print("Track test:")
-print(sp.track('514joG57v4yKTsfQmz7stz').get('name'))
